0c21af7c81b7f2efd7237e714e2cbe629150e102
Patrick Schadler <patrick.schadler@gmail.com>
2018-06-29 17:31:02 +0200

    refactor(people): removed people endpoint and logic

--- main.go
+++ main.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"net/http"
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 )
 
-var people []Person
-
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-}
-
-func provideTestPeople(){
-	people = append(people, Person{ID: "1", Firstname: "Patrick", Lastname: "Schadler"})
-	people = append(people, Person{ID: "2", Firstname: "Frodo", Lastname: "Beutlin"})
-	people = append(people, Person{ID: "3", Firstname: "Samweis", Lastname: "Gamdschie"})
-}
 
 func main() {
-	provideTestPeople()
 
 	router := mux.NewRouter()
-	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/addTwoNumber", AddTwoNumbersHandler)
 	router.HandleFunc("/squareNumber", SquareHandler)
 	router.HandleFunc("/divideNumber", DivideNumbersHandler)
